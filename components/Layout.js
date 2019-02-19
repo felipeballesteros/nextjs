1f08c34c395e00b0f73959eed8c079d7eb8d3330
felipe <felipe.ballesteros_perdomo@telus.com>
2019-02-18 20:44:45 -0500

    Adding head component, basic bootstrap style

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from './Header';
 
 const layoutStyle = {
@@ -6,10 +7,22 @@ const layoutStyle = {
   border: '1px solid #DDD'
 }
 
+// Adding some custom header values, adding bootstrap
+const CustomHead = () => (
+  <Head>
+    <title>Next JS Sample App</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous"></link>
+  </Head>
+)
+
 const Layout = (props) => (
   <div style={layoutStyle}>
-    <Header />
-    {props.children}
+    <CustomHead />
+    <div>
+      <Header />
+      {props.children}
+    </div>
   </div>
 )
 
