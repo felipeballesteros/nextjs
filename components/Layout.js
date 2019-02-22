46093fb910a6fdd2d496023020ad031f0f37f721
felipe <felipe.ballesteros_perdomo@telus.com>
2019-02-22 08:28:45 -0500

    Removing all styles, installed styled-components for styling later

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,16 @@
 import Head from 'next/head';
 import Header from './Header';
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-}
-
 // Adding some custom header values, adding bootstrap
 const CustomHead = () => (
   <Head>
     <title>Next JS Sample App</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous"></link>
   </Head>
 )
 
 const Layout = (props) => (
-  <div style={layoutStyle}>
+  <div>
     <CustomHead />
     <div>
       <Header />
